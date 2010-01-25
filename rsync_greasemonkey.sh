ab836438da422eb80df65a909d78599c11ba06f5
j138 <j138cm@gmail.com>
2010-01-25 22:02:15 +0900

    removed tmpfiles

--- rsync_greasemonkey.sh
+++ rsync_greasemonkey.sh
@@ -1,10 +1,12 @@
 rsync --progress --delete -avp ~/Library/Application\ Support/Firefox/Profiles/*/gm_scripts ~/Documents/dotfiles/firefox_profiles/
-rsync --progress --delete -avp ~/.vim ~/Documents/dotfiles/.vim/
-rsync --progress --delete -avp ~/.vimperator ~/Documents/dotfiles/.vimperator/
-rsync --progress --delete -avp ~/.irssi ~/Documents/dotfiles/.irssi/
+rsync --progress --delete -avp ~/.vim          ~/Documents/dotfiles/.vim/
+rsync --progress --delete -avp ~/.vimperator   ~/Documents/dotfiles/.vimperator/
+rsync --progress --delete -avp ~/.irssi        ~/Documents/dotfiles/.irssi/
 
-rsync --progress -avp ~/.vimrc          ~/Documents/dotfiles/.vimrc
-rsync --progress -avp ~/.vimperatorrc   ~/Documents/dotfiles/.vimperatorrc
-rsync --progress -avp ~/.zshrc          ~/Documents/dotfiles/.zshrc
-rsync --progress -avp ~/.gvimrc         ~/Documents/dotfiles/.gvimrc
-rsync --progress -avp ~/.screenrc       ~/Documents/dotfiles/.screenrc
+rsync --progress -avp ~/.vimrc                 ~/Documents/dotfiles/.vimrc
+rsync --progress -avp ~/.vimperatorrc          ~/Documents/dotfiles/.vimperatorrc
+rsync --progress -avp ~/.zshrc                 ~/Documents/dotfiles/.zshrc
+rsync --progress -avp ~/.gvimrc                ~/Documents/dotfiles/.gvimrc
+rsync --progress -avp ~/.screenrc              ~/Documents/dotfiles/.screenrc
+rsync --progress -avp ~/.gitconfig             ~/Documents/dotfiles/.gitignore
+rsync --progress -avp ~/rsync_myconfigdata.sh  ~/Documents/dotfiles/rsync_greasemonkey.sh
