a04cc6a485e7052119c72a2512e2cfb3c7a2bd82
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2014-11-10 17:17:00 +0900

    setup更新

--- setup.sh
+++ setup.sh
@@ -2,12 +2,18 @@
 DOTFILES=~/www/dotfiles
 ln -s $DOTFILES/.vim ~/.vim
 
+mkdir ~/tmp
+mkdir ~/log
 
 echo 'source ~/www/dotfiles/.vimrc' >> ~/.vimrc
 echo 'source ~/www/dotfiles/.vimperatorrc' >> ~/.vimperatorrc
 echo 'source ~/www/dotfiles/.zshrc' >> ~/.zshrc
 echo 'source ~/www/dotfiles/.zshenv' >> ~/.zshenv
 echo 'source-file ~/www/dotfiles/.tmux.conf' >> ~/.tmux.conf
+
+git config include.path=~/www/dotfiles/.gitconfig
+git user.name Junya Nakazato
+git user.email nakazato_junya@ca-adv.co.jp
 
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 git clone https://github.com/Shougo/vimproc.vim $DOTFILES/.vim/bundle/vimproc.vim
