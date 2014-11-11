4c4c919c948371221379d12c22e8928177ea77fb
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2014-11-11 17:35:14 +0900

    path調整

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-DOTFILES=~/src/dotfiles
+DOTFILES=\~/src/dotfiles
 ln -s $DOTFILES/.vim ~/.vim
 ln -s $DOTFILES/.zsh ~/.zsh
 
 mkdir ~/tmp
 mkdir ~/log
 
-echo 'source $DOTFILES/.vimrc' >> ~/.vimrc
-echo 'source $DOTFILES/.vimperatorrc' >> ~/.vimperatorrc
-echo 'source $DOTFILES/.zsh/.zshrc' >> ~/.zshrc
-echo 'source $DOTFILES/.zsh/.zshenv' >> ~/.zshenv
-echo 'source-file $DOTFILES/.tmux/main.conf' >> ~/.tmux.conf
+echo "source $DOTFILES/.vimrc" >> ~/.vimrc
+echo "source $DOTFILES/.vimperatorrc" >> ~/.vimperatorrc
+echo "source $DOTFILES/.zsh/.zshrc" >> ~/.zshrc
+echo "source $DOTFILES/.zsh/.zshenv" >> ~/.zshenv
+echo "source-file $DOTFILES/.tmux/main.conf" >> ~/.tmux.conf
 
 git config include.path=$DOTFILES/.gitconfig
 git user.name Junya Nakazato
