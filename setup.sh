c52a54a7955f7fd803f4a890086e1fa2469a7800
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2014-11-12 18:30:54 +0900

    fix git-config

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ echo "source $DOTFILES/.zsh/.zshenv" >> ~/.zshenv
 echo "source-file $DOTFILES/.tmux/main.conf" >> ~/.tmux.conf
 
 
-git config --global include.path=$DOTFILES/.gitconfig
+git config --global include.path $DOTFILES/.gitconfig
 git config --global user.name "Junya Nakazato"
 git config --global user.email nakazato_junya@ca-adv.co.jp
 
