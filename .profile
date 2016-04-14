53279ccd981e80b4fda43e9d5ead49e9aa7046a9
j138 <j138cm@gmail.com>
2016-04-14 11:14:49 +0900

    整理

--- .profile
+++
@@ -1,3 +0,0 @@
-path=(/usr/local/heroku/bin $path)
-source ~/nvm/nvm.sh
-nvm use v0.8.1 > /dev/null
--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-USER=`id -u`
+USER=$(id -u)
 DOTFILES=$HOME/src/dotfiles
 
-ln -s $DOTFILES/.vim ~/.vim
-ln -s $DOTFILES/.zsh ~/.zsh
-ln -s $DOTFILES/.tmux ~/.tmux
-mkdir $HOME/{tmp,log,.cache}
-
+ln -s "${DOTFILES}"/.vim ~/.vim
+ln -s "${DOTFILES}"/.zsh ~/.zsh
+ln -s "${DOTFILES}"/.tmux ~/.tmux
+mkdir "${HOME}"/{tmp,log,.cache}
 
 echo "source $DOTFILES/.vimrc" > ~/.vimrc
 echo "source $DOTFILES/.vimperatorrc" >> ~/.vimperatorrc
@@ -15,18 +14,8 @@ echo "source $DOTFILES/.zsh/.zshrc.mine" >> ~/.zshrc.mine
 echo "source $DOTFILES/.zsh/.zshenv" >> ~/.zshenv
 echo "source-file $DOTFILES/.tmux/main.conf" >> ~/.tmux.conf
 
-
-git config --global include.path $DOTFILES/.gitconfig
+git config --global include.path "${DOTFILES}"/.gitconfig
 git config --global user.name "Junya Nakazato"
 git config --global user.email nakazato_junya@ca-adv.co.jp
 
-
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-git clone https://github.com/Shougo/vimproc.vim $DOTFILES/.vim/bundle/vimproc.vim
-cd $DOTFILES/.vim/bundle/vimproc.vim/
-make -f ./make_unix.mak
-$DOTFILES/.vim/bundle/neobundle.vim/bin/neoinstall
-
-
-chown -R $USER $HOME/{tmp,log,.cache}
-
+chown -R "${USER}" "${HOME}"/{tmp,log,.cache}
