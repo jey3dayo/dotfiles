b3ea81be0402e9cc15cb89d6008017a16f263266
j138 <j138cm@gmail.com>
2016-11-11 13:52:16 +0900

    peco二オプション追加

--- .zsh/plugin-sources/select-history.zsh
+++ .zsh/plugin-sources/select-history.zsh
@@ -1,3 +1,5 @@
+OPTIONS='--select-1'
+
 function peco-select-history() {
     local tac
     if which tac > /dev/null; then
@@ -8,7 +10,7 @@ function peco-select-history() {
     BUFFER=$(history -n 1 | \
         eval $tac | \
         awk '!a[$0]++'  | \
-        peco --query "$LBUFFER")
+        peco "$OPTIONS" --query "$LBUFFER")
     CURSOR=$#BUFFER
     zle clear-screen
 }
