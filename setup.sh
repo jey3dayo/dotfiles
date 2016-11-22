9ad6f2620b8591fae950902b6bf25270af63adb3
j138 <j138cm@gmail.com>
2016-11-22 20:07:39 +0900

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -10,10 +10,12 @@ chown -R "${USER}" "${HOME}"/{tmp,.cache}
 
 echo "source $DOTFILES/.vimrc" > ~/.vimrc
 echo "source $DOTFILES/.vimperatorrc" >> ~/.vimperatorrc
-echo "source $DOTFILES/.zsh/.zshrc" > ~/.zshrc
-echo "source $DOTFILES/.zsh/.zshrc.mine" >> ~/.zshrc.mine
-echo "source $DOTFILES/.zsh/.zshenv" >> ~/.zshenv
 echo "source-file $DOTFILES/.tmux/main.conf" >> ~/.tmux.conf
+
+export ZDOTDIR=~/src/github.com/j138/dotfiles/.zsh
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+echo "export ZDOTDIR=~/src/github.com/j138/dotfiles/.zsh" >> ~/.zshenv
+echo "source $ZDOTDIR/.zshenv" >> ~/.zshenv
 
 git config --global include.path "${DOTFILES}"/.gitconfig
 git config --global user.name "Junya Nakazato"
