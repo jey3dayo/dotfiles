5dfbe48e2b14a4bb4a543340d407b7fc19fd5b21
j138 <j138cm@gmail.com>
2016-12-20 17:59:01 +0900

    do-enter.zsh

---
+++ .zsh/plugin-sources/do-enter.zsh
@@ -0,0 +1,20 @@
+do_enter() {
+    if [[ -n $BUFFER ]]; then
+        zle accept-line
+        return $status
+    fi
+
+    echo
+    if [[ -d .git ]]; then
+        if [[ -n "$(git status --short)" ]]; then
+            git status -sb
+        fi
+    else
+        # do nothing
+        :
+    fi
+
+    zle reset-prompt
+}
+zle -N do_enter
+bindkey '^m' do_enter
--- .zsh/plugin-sources/git-commands.zsh
+++ .zsh/plugin-sources/git-commands.zsh
@@ -1,9 +1,9 @@
-function _git_status() {
+function git_status() {
   if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
     echo git status -sb
     git status -sb
   fi
   zle reset-prompt
 }
-zle -N git_status _git_status
+zle -N git_status git_status
 bindkey '^G' git_status
