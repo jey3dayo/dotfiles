87c88713015cda9245d9dfbef965a3a4318a7896
j138 <j138cm@gmail.com>
2016-12-21 15:34:09 +0900

    ^G => git diff

---
+++ .zsh/plugin-sources/git-commands.zsh
@@ -0,0 +1,9 @@
+function git_diff() {
+  if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
+    echo git diff
+    git diff
+  fi
+  zle reset-prompt
+}
+zle -N git_diff git_diff
+bindkey '^G' git_diff
