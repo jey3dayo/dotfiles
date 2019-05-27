7e7e3a313370ad1ae4f315019026a56d09b595f5
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2019-05-27 17:03:48 +0900

    path修正

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ chown -R "${USER}" "${HOME}"/{tmp,.cache}
 
 ln -s "${DOTFILES}"/.vim ~/.config/nvim
 ln -s "${DOTFILES}"/.vim ~/.vim
-ln -s "${DOTFILES}"/.zsh ~/.zsh
+ln -s "${DOTFILES}"/.zsh ~/.config/.zsh
 ln -s "${DOTFILES}"/.tmux ~/.tmux
 ln -s "${DOTFILES}"/.config/powerline ~/.config/powerline
 ln -s "${DOTFILES}"/.config/.ignore ~/.ignore
@@ -16,7 +16,7 @@ echo "source $DOTFILES/.vimrc" > ~/.vimrc
 echo "source $DOTFILES/.vimperatorrc" >> ~/.vimperatorrc
 echo "source-file $DOTFILES/.tmux/main.conf" >> ~/.tmux.conf
 
-export ZDOTDIR=~/src/github.com/j138/dotfiles/.zsh
+export ZDOTDIR=~/src/github.com/.config/.zsh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 echo "export ZDOTDIR=$DOTFILES/.zsh" >> ~/.zshenv
 echo "source $ZDOTDIR/.zshenv" >> ~/.zshenv
