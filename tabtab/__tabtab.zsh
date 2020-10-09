bbc21ad32e538e5d6eb8581a471f337f08fac033
jey3dayo <nakazato_junya@ca-adv.co.jp>
2020-10-09 10:34:43 +0900

    sls向けパッケージtabtab追加

---
+++ tabtab/__tabtab.zsh
@@ -0,0 +1,8 @@
+
+# tabtab source for serverless package
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/serverless.zsh ]] && . ~/.config/tabtab/serverless.zsh || true
+
+# tabtab source for sls package
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/sls.zsh ]] && . ~/.config/tabtab/sls.zsh || true
