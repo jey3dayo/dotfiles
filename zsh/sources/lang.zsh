dad7213a6fa945dc12ab28399415f476cfe94338
jey3dayo <nakazato_junya@ca-adv.co.jp>
2022-11-30 11:26:26 +0900

    不要なENV整理

--- zsh/sources/lang.zsh
+++ zsh/sources/lang.zsh
@@ -1,17 +1,4 @@
-if [ -d $HOME/perl5 ] ; then
-  export PERL_LOCAL_LIB_ROOT="$HOME/perl5:$PERL_LOCAL_LIB_ROOT"
-  export PERL_MB_OPT="--install_base "$HOME/perl5""
-  export PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"
-  export PERL5LIB="$HOME/perl5/lib/perl5:$PERL5LIB"
-  path=($HOME/perl5/bin(N-/) $path)
-fi
-
-path=("$HOME/.cargo/bin/"(N-/) $path)
-
-# export JAVA_HOME=`/usr/libexec/java_home -v 1.8.0`
-export JAVA_HOME=`/usr/libexec/java_home -v 18`
 export STUDIO_JDK=${JAVA_HOME%/*/*}
-
 export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
 path=(
   $ANDROID_SDK_ROOT/emulator(N-/)
