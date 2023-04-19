bf7c625c852b65afea6cfd17aad4b4c1ef8f5453
jey3dayo <nakazato_junya@ca-adv.co.jp>
2023-04-20 00:43:35 +0900

    diffviewのconfigにcmd入れた

--- .vim/lua/plugins/tool.lua
+++ .vim/lua/plugins/tool.lua
@@ -9,6 +9,7 @@ return {
   {
     "sindrets/diffview.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
     config = function()
       require "config/diffview"
     end,
