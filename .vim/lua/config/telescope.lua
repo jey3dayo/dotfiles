26f3ea206634e80d2926aea6ba76743d640f19ba
jey3dayo <nakazato_junya@ca-adv.co.jp>
2023-08-23 12:18:21 +0900

    feat(telescope): add custom find_files picker configuration

--- .vim/lua/config/telescope.lua
+++ .vim/lua/config/telescope.lua
@@ -28,6 +28,12 @@ telescope.setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+    },
+  },
   extensions = {
     file_browser = {
       theme = "dropdown",
