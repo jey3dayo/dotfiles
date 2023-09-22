2cdbb2cc0f3ffa6700073b767b02110b5edf7b57
jey3dayo <nakazato_junya@ca-adv.co.jp>
2023-09-22 17:41:50 +0900

    replaced sheldon from zinit

--- zsh/sources/zinit.zsh
+++ zsh/sources/zinit.zsh
@@ -1,6 +1,6 @@
-if [[ -r "${ZDOTDIR:-$HOME}/.zinit.zsh" ]]; then
-  source "${ZDOTDIR:-$HOME}/.zinit.zsh"
-else
-  autoload -Uz compinit
-  compinit
-fi
+# if [[ -r "${ZDOTDIR:-$HOME}/.zinit.zsh" ]]; then
+#   source "${ZDOTDIR:-$HOME}/.zinit.zsh"
+# else
+#   autoload -Uz compinit
+#   compinit
+# fi
