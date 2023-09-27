a13d127f1d213993c6c67df101f6cff6bf703d0f
jey3dayo <nakazato_junya@ca-adv.co.jp>
2023-09-27 12:44:54 +0900

    refactor(plugins.toml): Remove unnecessary code and use hooks for post-processing

---
+++ zsh/hook/anyframe_post.zsh
@@ -0,0 +1,9 @@
+bindkey '^gh' anyframe-widget-execute-history
+bindkey '^gi' anyframe-widget-put-history
+bindkey '^gr' anyframe-widget-cd-ghq-repository
+bindkey '^gb' anyframe-widget-checkout-git-branch
+bindkey '^gk' anyframe-widget-kill
+
+bindkey '^R' anyframe-widget-execute-history
+bindkey '^T' anyframe-widget-cd-ghq-repository
+bindkey '^Y' anyframe-widget-checkout-git-branch
---
+++ zsh/hook/zoxide_post.zsh
@@ -0,0 +1,3 @@
+alias j=z
+export _ZO_RESOLVE_SYMLINKS=1
+eval "$(zoxide init zsh)"
