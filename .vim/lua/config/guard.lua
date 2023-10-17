11ed7d5239e470048dd4d5058a68b2f65610cc5d
jey3dayo <nakazato_junya@ca-adv.co.jp>
2023-10-17 12:07:37 +0900

    chore: add guard for lua and update package dependencies

--- .vim/lua/config/guard.lua
+++ .vim/lua/config/guard.lua
@@ -2,6 +2,7 @@ local ft = require("guard.filetype")
 
 -- ft("go"):fmt("gofmt")
 -- ft("ruby"):fmt("rubocop"):lint "rubocop"
+-- ft('lua'):fmt('lsp'):append('stylua')
 ft("sql,pgsql"):fmt("sql-formatter")
 
 ft('typescript,javascript,typescriptreact,css,json,astro'):fmt('prettier')
--- sh/update-pkg.sh
+++ sh/update-pkg.sh
@@ -7,6 +7,7 @@ sheldon lock --update
 # python
 pip3 install --upgrade pip
 pip3 list --format json --outdated | jq .[].name | xargs pip3 install -U
+# pipx reinstall-all
 pipx upgrade-all
 
 # node
@@ -19,4 +20,4 @@ cd /Users/t00114/src/github.com/dimdenGD/OldTweetDeck && git pull origin main &&
 brew update
 brew upgrade
 brew cleanup
-softwareupdate --all --install --force
+# softwareupdate --all --install --force
