48c8eb4f7c4f9322dbec81173b2656ebd26dae1b
jey3dayo <nakazato_junya@ca-adv.co.jp>
2023-11-13 11:33:25 +0900

    chore: housekeeping and updated dependencies

--- .vim/lua/config/conform.lua
+++ .vim/lua/config/conform.lua
@@ -3,6 +3,8 @@ if not status then
   return
 end
 
+local prettier = { { "prettierd", "prettier" } }
+
 conform.setup {
   format_on_save = {
     -- These options will be passed to conform.format()
@@ -12,9 +14,13 @@ conform.setup {
   formatters_by_ft = {
     lua = { "stylua" },
     go = { "gofmt" },
-    javascriptreact = { { "prettierd", "prettier" } },
-    typescriptreact = { { "prettierd", "prettier" } },
     python = { "isort", "black" },
     sql = { "sql_formatter" },
+    json = prettier,
+    jsonc = prettier,
+    javascriptreact = prettier,
+    typescriptreact = prettier,
+    javascript = prettier,
+    typescript = prettier,
   },
 }
