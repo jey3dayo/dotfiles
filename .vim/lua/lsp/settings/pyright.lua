d6008fba4303c577a7efbf75bc1e532756ce3b52
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-02-22 16:53:33 +0900

    feat: Add pyright settings for Python LSP

---
+++ .vim/lua/lsp/settings/pyright.lua
@@ -0,0 +1,11 @@
+return {
+  settings = {
+    python = {
+      venvPath = ".",
+      pythonPath = "./.venv/bin/python",
+      analysis = {
+        extraPaths = { "." },
+      },
+    },
+  },
+}
