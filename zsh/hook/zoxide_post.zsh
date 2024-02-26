98eddb2e1855c711069fda9b8994e13ddce6b420
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-02-26 16:52:29 +0900

    chore: removed unused files

--- zsh/hook/zoxide_post.zsh
+++
@@ -1,3 +0,0 @@
-alias j=z
-export _ZO_RESOLVE_SYMLINKS=1
-eval "$(zoxide init zsh)"
--- zsh/sources/zinit.zsh
+++
@@ -1,8 +0,0 @@
-return
-
-if [[ -r "${ZDOTDIR:-$HOME}/.zinit.zsh" ]]; then
-  source "${ZDOTDIR:-$HOME}/.zinit.zsh"
-else
-  autoload -Uz compinit
-  compinit
-fi
