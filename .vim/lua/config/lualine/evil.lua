53e8f9dcadda8befda21f1000977ed536c462ff8
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-05-22 18:43:17 +0900

    style: update quotes to use double quotes in Lua code

--- .vim/lua/config/lualine/evil.lua
+++ .vim/lua/config/lualine/evil.lua
@@ -25,13 +25,13 @@ local mode_color = {
   n = colors.primary,
   i = colors.green,
   v = colors.blue,
-  [''] = colors.blue,
+  [""] = colors.blue,
   V = colors.blue,
   c = colors.magenta,
   no = colors.red,
   s = colors.orange,
   S = colors.orange,
-  [''] = colors.orange,
+  [""] = colors.orange,
   ic = colors.yellow,
   R = colors.violet,
   Rv = colors.violet,
@@ -39,21 +39,21 @@ local mode_color = {
   ce = colors.red,
   r = colors.cyan,
   rm = colors.cyan,
-  ['r?'] = colors.cyan,
-  ['!'] = colors.red,
+  ["r?"] = colors.cyan,
+  ["!"] = colors.red,
   t = colors.red,
 }
 
 local conditions = {
   buffer_not_empty = function()
-    return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+    return vim.fn.empty(vim.fn.expand "%:t") ~= 1
   end,
   hide_in_width = function()
     return vim.fn.winwidth(0) > 80
   end,
   check_git_workspace = function()
-    local filepath = vim.fn.expand('%:p:h')
-    local gitdir = vim.fn.finddir('.git', filepath .. ';')
+    local filepath = vim.fn.expand "%:p:h"
+    local gitdir = vim.fn.finddir(".git", filepath .. ";")
     return gitdir and #gitdir > 0 and #gitdir < #filepath
   end,
 }
@@ -62,8 +62,8 @@ local conditions = {
 local config = {
   options = {
     -- Disable sections and component separators
-    component_separators = '',
-    section_separators = '',
+    component_separators = "",
+    section_separators = "",
     theme = {
       -- We are going to use lualine_c an lualine_x as left and
       -- right section. Both are highlighted by c theme .  So we
@@ -107,7 +107,7 @@ end
 
 ins_left {
   function()
-    return '▊'
+    return "▊"
   end,
   color = function()
     return { fg = mode_color[vim.fn.mode()] }
@@ -118,7 +118,7 @@ ins_left {
 ins_left {
   -- mode component
   function()
-    return ''
+    return ""
   end,
   color = function()
     return { fg = mode_color[vim.fn.mode()] }
@@ -128,24 +128,24 @@ ins_left {
 
 ins_left {
   -- filesize component
-  'filesize',
+  "filesize",
   cond = conditions.buffer_not_empty,
 }
 
 ins_left {
-  'filename',
+  "filename",
   cond = conditions.buffer_not_empty,
-  color = { fg = colors.primary, gui = 'bold' },
+  color = { fg = colors.primary, gui = "bold" },
 }
 
-ins_left { 'location' }
+ins_left { "location" }
 
-ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
+ins_left { "progress", color = { fg = colors.fg, gui = "bold" } }
 
 ins_left {
-  'diagnostics',
-  sources = { 'nvim_diagnostic' },
-  symbols = { error = ' ', warn = ' ', info = ' ' },
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  symbols = { error = " ", warn = " ", info = " " },
   diagnostics_color = {
     color_error = { fg = colors.red },
     color_warn = { fg = colors.yellow },
@@ -157,16 +157,16 @@ ins_left {
 -- for lualine it's any number greater then 2
 ins_left {
   function()
-    return '%='
+    return "%="
   end,
 }
 
 ins_left {
   -- Lsp server name .
   function()
-    local msg = 'N/A'
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
+    local msg = "N/A"
+    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+    local clients = vim.lsp.get_clients()
     if next(clients) == nil then
       return msg
     end
@@ -178,35 +178,35 @@ ins_left {
     end
     return msg
   end,
-  icon = ' LSP:',
-  color = { fg = colors.fg, gui = 'bold' },
+  icon = " LSP:",
+  color = { fg = colors.fg, gui = "bold" },
 }
 
 -- Add components to right sections
 ins_right {
-  'o:encoding',       -- option component same as &encoding in viml
+  "o:encoding", -- option component same as &encoding in viml
   fmt = string.upper, -- I'm not sure why it's upper case either ;)
   cond = conditions.hide_in_width,
-  color = { fg = colors.green, gui = 'bold' },
+  color = { fg = colors.green, gui = "bold" },
 }
 
 ins_right {
-  'fileformat',
+  "fileformat",
   fmt = string.upper,
   icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-  color = { fg = colors.green, gui = 'bold' },
+  color = { fg = colors.green, gui = "bold" },
 }
 
 ins_right {
-  'branch',
-  icon = '',
-  color = { fg = colors.violet, gui = 'bold' },
+  "branch",
+  icon = "",
+  color = { fg = colors.violet, gui = "bold" },
 }
 
 ins_right {
-  'diff',
+  "diff",
   -- Is it me or the symbol for modified us really weird
-  symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
+  symbols = { added = " ", modified = "󰝤 ", removed = " " },
   diff_color = {
     added = { fg = colors.green },
     modified = { fg = colors.orange },
@@ -217,10 +217,10 @@ ins_right {
 
 ins_right {
   function()
-    return '▊'
+    return "▊"
   end,
   color = { fg = colors.primary },
   padding = { left = 1 },
 }
 
-return config;
+return config
