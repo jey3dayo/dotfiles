4992a09c7489063a7440fc04558c50734e0d9426
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-05-29 12:56:00 +0900

    feat: Add keymaps for updating source and sourcing config

--- .vim/lua/keymaps.lua
+++ .vim/lua/keymaps.lua
@@ -52,14 +52,26 @@ Keymap("<C-t>p", ":<C-u>tabprevious<CR>")
 Keymap("gt", ":<C-u>tabnext<CR>")
 Keymap("gT", ":<C-u>tabprevious<CR>")
 
--- source cmd
-Keymap("<Leader>so", ":<C-u>source " .. vim.fn.stdpath "config" .. "/init.lua<CR>")
-Keymap("<Leader>su", ":<C-u>Lazy update<CR>")
-
 -- set list
 Keymap("<Leader>sn", ":<C-u>set number!<CR>")
 Keymap("<Leader>sl", ":<C-u>set list!<CR>")
 Keymap("<leader><C-d>", ":<C-u>bd!<CR>")
 
+-- update source
+Keymap("<Leader>su", ":<C-u>Lazy update<CR>")
+
+-- source config
+local function source_config()
+  local config_path = vim.fn.stdpath "config" .. "/init.lua"
+  vim.cmd("source " .. config_path)
+  vim.api.nvim_echo({ { "Loaded config: " .. config_path, "None" } }, true, {})
+end
+Keymap("<Leader>so", source_config)
+
 -- yank
-Keymap("<Leader>y", ":let @* = expand('%:.')<CR>")
+local function copy_path()
+  local path = vim.fn.expand "%:."
+  vim.fn.setreg("*", path)
+  vim.api.nvim_echo({ { "Copied: " .. path, "None" } }, true, {})
+end
+Keymap("<Leader>y", copy_path)
