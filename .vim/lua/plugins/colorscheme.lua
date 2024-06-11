fa6005d3f7e781f5e56fe9daa6178dedc07d26a4
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-06-11 14:30:26 +0900

    feat: Add folke/tokyonight.nvim colorscheme with options

--- .vim/lua/plugins/colorscheme.lua
+++ .vim/lua/plugins/colorscheme.lua
@@ -3,4 +3,14 @@ return {
   "rebelot/kanagawa.nvim",
   "joshdick/onedark.vim",
   "cocopon/iceberg.vim",
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 }
