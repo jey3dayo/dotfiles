4182b748a0cccb0b1150d7532c374956142289d5
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-06-20 13:46:33 +0900

    feat: Add key bindings to LSP Saga finder

--- .vim/lua/config/lspsaga.lua
+++ .vim/lua/config/lspsaga.lua
@@ -1,4 +1,12 @@
 return {
+  finder = {
+    keys = {
+      toggle_or_open = "<CR>",
+      shuttle = "<Tab>",
+      vsplit = "v",
+      split = "s",
+    },
+  },
   code_action_lightbulb = {
     enable = true,
   },
