578a3e48e512ea15472d03f29dbfb118ff588e0b
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-09-27 18:15:38 +0900

    feat(plugins): add flash.nvim and disable two plugins

--- .vim/lua/plugins/input.lua
+++ .vim/lua/plugins/input.lua
@@ -27,12 +27,12 @@ return {
   },
   {
     "ggandor/lightspeed.nvim",
-    enabled = true,
+    enabled = false,
     config = true,
   },
   {
     "ggandor/leap.nvim",
-    enabled = true,
+    enabled = false,
     config = function()
       require "config/leap"
     end,
@@ -44,6 +44,19 @@ return {
     config = function()
       require "config/hop"
     end,
+  },
+  {
+    "folke/flash.nvim",
+    enabled = true,
+    event = "VeryLazy",
+    keys = {
+      { "t", mode = { "n", "x", "o" }, false },
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
   {
     "Wansmer/treesj",
