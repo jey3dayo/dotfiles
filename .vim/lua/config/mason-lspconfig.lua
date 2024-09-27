779c4407ada5bed4161c310a39e3c59213f08b89
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-09-27 15:08:19 +0900

    refactor(lsp): modularize EFM language configs

--- .vim/lua/config/mason-lspconfig.lua
+++ .vim/lua/config/mason-lspconfig.lua
@@ -6,27 +6,6 @@ if not (mason_lspconfig and lspconfig) then
   return
 end
 
-local biome = require "efmls-configs.formatters.biome"
-local eslint = require "efmls-configs.linters.eslint"
-local fixjson = require "efmls-configs.formatters.fixjson"
-local hadolint = require "efmls-configs.linters.hadolint"
-local markdownlint = require "efmls-configs.linters.markdownlint"
-local prettier = require "efmls-configs.formatters.prettier"
-local ruff_formatter = require "efmls-configs.formatters.ruff"
-local ruff_linter = require "efmls-configs.linters.ruff"
-local ruff_sort = require "efmls-configs.formatters.ruff_sort"
-local shellcheck = require "efmls-configs.linters.shellcheck"
-local shfmt = require "efmls-configs.formatters.shfmt"
-local stylua = require "efmls-configs.formatters.stylua"
-local taplo = require "efmls-configs.formatters.taplo"
-local vint = require "efmls-configs.linters.vint"
-local yamllint = require "efmls-configs.linters.yamllint"
-local codespell = require "efmls-configs.linters.codespell"
-local gitlint = require "efmls-configs.linters.gitlint"
-local jsonlint = require "efmls-configs.linters.jsonlint"
-local luacheck = require "efmls-configs.linters.luacheck"
-local gofmt = require "efmls-configs.formatters.gofmt"
-
 mason_lspconfig.setup {
   ensure_installed = require("lsp.config").installed_servers,
   automatic_installation = true,
@@ -41,32 +20,14 @@ mason_lspconfig.setup_handlers {
       on_attach = on_attach,
       capabilities = capabilities,
     }
-    local server_custom_opts = safe_require("lsp.settings." .. server)
+    local extends = safe_require("lsp.settings." .. server)
 
-    lspconfig[server].setup(with(opts, server_custom_opts))
+    lspconfig[server].setup(with(opts, extends))
   end,
 }
 
-local languages = {
-  javascript = { biome, eslint, prettier },
-  typescript = { biome, eslint, prettier },
-  html = { prettier },
-  css = { prettier },
-  python = { ruff_linter, ruff_formatter, ruff_sort },
-  lua = { stylua, luacheck },
-  markdown = { markdownlint },
-  dockerfile = { hadolint },
-  json = { fixjson, jsonlint },
-  yaml = { yamllint },
-  sh = { shellcheck, shfmt },
-  vim = { vint },
-  toml = { taplo },
-  go = { gofmt },
-  gitcommit = { gitlint },
-  text = { codespell },
-}
-
-local efmls_config = {
+local languages = require("lsp.config").efm_languages
+lspconfig.efm.setup {
   filetypes = vim.tbl_keys(languages),
   settings = {
     rootMarkers = { ".git/" },
@@ -74,11 +35,8 @@ local efmls_config = {
   },
   init_options = {
     documentFormatting = true,
-    -- documentRangeFormatting = true,
+    documentRangeFormatting = true,
   },
-}
-lspconfig.efm.setup(with(efmls_config, {
-  -- Pass your custom lsp config below like on_attach and capabilities
-      on_attach = on_attach,
+  on_attach = on_attach,
   capabilities = capabilities,
-}))
+}
--- .vim/lua/lsp/config.lua
+++ .vim/lua/lsp/config.lua
@@ -1,3 +1,24 @@
+local biome = require "efmls-configs.formatters.biome"
+local eslint = require "efmls-configs.linters.eslint"
+local fixjson = require "efmls-configs.formatters.fixjson"
+local hadolint = require "efmls-configs.linters.hadolint"
+local markdownlint = require "efmls-configs.linters.markdownlint"
+local prettier = require "efmls-configs.formatters.prettier"
+local ruff_formatter = require "efmls-configs.formatters.ruff"
+local ruff_linter = require "efmls-configs.linters.ruff"
+local ruff_sort = require "efmls-configs.formatters.ruff_sort"
+local shellcheck = require "efmls-configs.linters.shellcheck"
+local shfmt = require "efmls-configs.formatters.shfmt"
+local stylua = require "efmls-configs.formatters.stylua"
+local taplo = require "efmls-configs.formatters.taplo"
+local vint = require "efmls-configs.linters.vint"
+local yamllint = require "efmls-configs.linters.yamllint"
+local codespell = require "efmls-configs.linters.codespell"
+local gitlint = require "efmls-configs.linters.gitlint"
+local jsonlint = require "efmls-configs.linters.jsonlint"
+local luacheck = require "efmls-configs.linters.luacheck"
+local gofmt = require "efmls-configs.formatters.gofmt"
+
 local M = {}
 
 M.installed_servers = {
@@ -42,6 +63,25 @@ M.installed_tree_sitter = {
   "typescript",
   "vim",
   "yaml",
+}
+
+M.efm_languages = {
+  javascript = { biome, eslint, prettier },
+  typescript = { biome, eslint, prettier },
+  html = { prettier },
+  css = { prettier },
+  python = { ruff_linter, ruff_formatter, ruff_sort },
+  lua = { stylua, luacheck },
+  markdown = { markdownlint },
+  dockerfile = { hadolint },
+  json = { fixjson, jsonlint },
+  yaml = { yamllint },
+  sh = { shellcheck, shfmt },
+  vim = { vint },
+  toml = { taplo },
+  go = { gofmt },
+  gitcommit = { gitlint },
+  text = { codespell },
 }
 
 return M
