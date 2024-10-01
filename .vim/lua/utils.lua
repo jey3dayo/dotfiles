7703ce325ca8dffaa1781de12ad3fa5acc701af0
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-10-01 15:37:51 +0900

    feat(utils): Add table_to_string and print_map functions

--- .vim/lua/utils.lua
+++ .vim/lua/utils.lua
@@ -28,13 +28,45 @@ M.get_git_dir = function()
   return git_dir
 end
 
-
-M.with = function (tbl, extend)
+M.with = function(tbl, extend)
   if not extend or vim.tbl_isempty(extend) then
     return tbl
   end
 
   return vim.tbl_extend("force", tbl, extend)
+end
+
+local function table_to_string(tbl, indent)
+  if not indent then
+    indent = 0
+  end
+  local toprint = string.rep(" ", indent) .. "{\n"
+  indent = indent + 2
+  for k, v in pairs(tbl) do
+    toprint = toprint .. string.rep(" ", indent)
+    if type(k) == "number" then
+      toprint = toprint .. "[" .. k .. "] = "
+    elseif type(k) == "string" then
+      toprint = toprint .. k .. "= "
+    end
+    if type(v) == "number" then
+      toprint = toprint .. v .. ",\n"
+    elseif type(v) == "string" then
+      toprint = toprint .. '"' .. v .. '",\n'
+    elseif type(v) == "table" then
+      toprint = toprint .. table_to_string(v, indent + 2) .. ",\n"
+    else
+      toprint = toprint .. '"' .. tostring(v) .. '",\n'
+    end
+  end
+  toprint = toprint .. string.rep(" ", indent - 2) .. "}"
+  return toprint
+end
+
+M.table_to_string = table_to_string
+
+M.print_map = function(tab)
+  print(table_to_string(tab))
 end
 
 return M
