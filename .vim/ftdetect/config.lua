237490599426c23f5263f8c86af90e8aef3e34b2
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-10-21 12:41:39 +0900

    feat(vim): add config filetype detection for .env files

---
+++ .vim/ftdetect/config.lua
@@ -0,0 +1,6 @@
+vim.filetype.add {
+  pattern = {
+    [".env*"] = "config",
+    [".env.*"] = "config",
+  },
+}
--- .vim/ftdetect/zsh.lua
+++ .vim/ftdetect/zsh.lua
@@ -1,6 +1,5 @@
 vim.filetype.add {
   pattern = {
-    [".env*"] = "zsh",
     [".envrc"] = "zsh",
     ["abbreviations"] = "zsh",
   },
