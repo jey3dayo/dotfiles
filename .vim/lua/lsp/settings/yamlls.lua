32897b63d8794c9dbb484611ed77adbe8b332658
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-10-25 01:17:16 +0900

    fix(yamlls): update schema URLs to use HTTPS

--- .vim/lua/lsp/settings/yamlls.lua
+++ .vim/lua/lsp/settings/yamlls.lua
@@ -3,7 +3,7 @@ return {
     yaml = {
       -- Schemas https://www.schemastore.org
       schemas = {
-        ["http://json.schemastore.org/gitlab-ci.json"] = {
+        ["https://json.schemastore.org/gitlab-ci.json"] = {
           ".gitlab-ci.yml",
         },
         ["https://json.schemastore.org/bamboo-spec.json"] = {
@@ -12,11 +12,11 @@ return {
         ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
           "docker-compose*.{yml,yaml}",
         },
-        ["http://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
-        ["http://json.schemastore.org/github-action.json"] = ".github/action.{yml,yaml}",
-        ["http://json.schemastore.org/prettierrc.json"] = ".prettierrc.{yml,yaml}",
-        ["http://json.schemastore.org/stylelintrc.json"] = ".stylelintrc.{yml,yaml}",
-        ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+        ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
+        ["https://json.schemastore.org/github-action.json"] = ".github/action.{yml,yaml}",
+        ["https://json.schemastore.org/prettierrc.json"] = ".prettierrc.{yml,yaml}",
+        ["https://json.schemastore.org/stylelintrc.json"] = ".stylelintrc.{yml,yaml}",
+        ["https://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
       },
     },
   },
