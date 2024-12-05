6c1e762d54df07502db4f5de56f87322293f842f
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-12-05 12:30:32 +0900

    feat(nvim-treesitter): add markdown regex highlighting

--- .vim/lua/config/nvim-treesitter.lua
+++ .vim/lua/config/nvim-treesitter.lua
@@ -1,5 +1,8 @@
 return {
-  highlight = { enable = true, disable = {} },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = { "markdown" },
+  },
   indent = { enable = true, disable = {} },
   ensure_installed = require("lsp.config").installed_tree_sitter,
   autotag = { enable = true },
