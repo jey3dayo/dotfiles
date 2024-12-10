447be5a4f9e05e718f27c75c0e6e06d031493a98
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-12-10 17:20:23 +0900

    refactor(yamlls): extract YAML schemas into a variable

--- .vim/lua/lsp/settings/yamlls.lua
+++ .vim/lua/lsp/settings/yamlls.lua
@@ -1,23 +1,29 @@
+local util = require "lspconfig.util"
+
+local schemas = {
+  schemas = {
+    ["https://json.schemastore.org/gitlab-ci.json"] = {
+      ".gitlab-ci.yml",
+    },
+    ["https://json.schemastore.org/bamboo-spec.json"] = {
+      "bamboo-specs/*.{yml,yaml}",
+    },
+    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
+      "docker-compose*.{yml,yaml}",
+    },
+    ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
+    ["https://json.schemastore.org/github-action.json"] = ".github/action.{yml,yaml}",
+    ["https://json.schemastore.org/prettierrc.json"] = ".prettierrc.{yml,yaml}",
+    ["https://json.schemastore.org/stylelintrc.json"] = ".stylelintrc.{yml,yaml}",
+    ["https://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+  },
+}
+
 return {
+  filetypes = { "yml", "yaml" },
   settings = {
     yaml = {
-      -- Schemas https://www.schemastore.org
-      schemas = {
-        ["https://json.schemastore.org/gitlab-ci.json"] = {
-          ".gitlab-ci.yml",
-        },
-        ["https://json.schemastore.org/bamboo-spec.json"] = {
-          "bamboo-specs/*.{yml,yaml}",
-        },
-        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
-          "docker-compose*.{yml,yaml}",
-        },
-        ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
-        ["https://json.schemastore.org/github-action.json"] = ".github/action.{yml,yaml}",
-        ["https://json.schemastore.org/prettierrc.json"] = ".prettierrc.{yml,yaml}",
-        ["https://json.schemastore.org/stylelintrc.json"] = ".stylelintrc.{yml,yaml}",
-        ["https://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
-      },
+      schemas = schemas,
     },
   },
 }
