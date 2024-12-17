55aa61bb68bf6ca9b177004004e6af27b95e1fe4
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-12-17 11:41:18 +0900

    Remove copilot.lua dependency from avante plugin

--- .vim/lua/plugins/avante.lua
+++ .vim/lua/plugins/avante.lua
@@ -27,7 +27,6 @@ return {
       --- The below dependencies are optional,
       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
