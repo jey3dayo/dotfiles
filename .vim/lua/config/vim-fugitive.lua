32e89a6968fcc8bff650c0a6136708c111bcfd10
jey3dayo <nakazato_junya@ca-adv.co.jp>
2025-01-16 18:54:25 +0900

    refactor(vim-fugitive): update git keymaps configuration

--- .vim/lua/config/vim-fugitive.lua
+++ .vim/lua/config/vim-fugitive.lua
@@ -5,15 +5,15 @@ local set_opts = { silent = true }
 Set_keymap("[git]", "<Nop>", set_opts)
 Set_keymap("<C-g>", "[git]", set_opts)
 
+Keymap("[git]<C-g>", "<cmd>DiffviewOpen<CR>")
+
 Keymap("[git]b", "<cmd>Git blame<CR>")
-Keymap("[git]d", "<cmd>DiffviewOpen<CR>")
+Keymap("[git]d", "<cmd>Gdiffsplit<CR>")
 Keymap("[git]h", "<cmd>DiffviewFileHistory<CR>")
 Keymap("[git]H", "<cmd>DiffviewFileHistory %<CR>")
-Keymap("[git]D", "<cmd>Gdiffsplit<CR>")
 Keymap("[git]e", "<cmd>DiffviewFocusFiles<CR>")
 Keymap("[git]r", "<cmd>DiffviewRefresh<CR>")
 Keymap("[git]g", "<cmd>Ggrep<Space>")
-Keymap("[git]s", "<cmd>Git status<CR>")
 Keymap("[git]w", "<cmd>Gwrite<CR>")
 Keymap("[git]x", "<cmd>DiffviewClose<CR>")
 Keymap(
