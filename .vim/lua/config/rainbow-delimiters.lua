03e2be946ec80f9f272531f2a16b3a6cf3a04ade
jey3dayo <nakazato_junya@ca-adv.co.jp>
2025-03-18 11:41:52 +0900

    refactor(vim): update rainbow-delimiters setup syntax

--- .vim/lua/config/rainbow-delimiters.lua
+++ .vim/lua/config/rainbow-delimiters.lua
@@ -1,12 +1,16 @@
--- This module contains a number of default definitions
-local rainbow_delimiters = require "rainbow-delimiters"
-
-vim.g.rainbow_delimiters = {
+require("rainbow-delimiters.setup").setup {
   strategy = {
-    [""] = rainbow_delimiters.strategy["global"],
-    vim = rainbow_delimiters.strategy["local"],
+    [""] = "rainbow-delimiters.strategy.global",
+    vim = "rainbow-delimiters.strategy.local",
   },
-  query = { [""] = "rainbow-delimiters", lua = "rainbow-blocks" },
+  query = {
+    [""] = "rainbow-delimiters",
+    lua = "rainbow-blocks",
+  },
+  priority = {
+    [""] = 110,
+    lua = 210,
+  },
   highlight = {
     "RainbowDelimiterRed",
     "RainbowDelimiterYellow",
