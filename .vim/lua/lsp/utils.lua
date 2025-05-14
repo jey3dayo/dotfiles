88204739a37249a4ed139caa62fa96171419d384
jey3dayo <nakazato_junya@ca-adv.co.jp>
2025-05-14 19:21:38 +0900

    refactor(lsp): use local unpack for root pattern function

--- .vim/lua/lsp/utils.lua
+++ .vim/lua/lsp/utils.lua
@@ -1,10 +1,11 @@
 local util = require "lspconfig.util"
+local unpack = table.unpack or unpack
 
 local M = {}
 
 M.create_root_pattern = function(patterns)
   return function(fname)
-    return util.root_pattern(table.unpack(patterns))(fname)
+    return util.root_pattern(unpack(patterns))(fname)
   end
 end
 
