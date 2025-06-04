079ca9a88f3dca14a0da86d12ea7fe0e8bac72e5
jey3dayo <nakazato_junya@ca-adv.co.jp>
2025-06-04 16:36:41 +0900

    refactor(zsh): update zshrc and abbreviations setup

--- zsh/lazy-sources/abbreviations.zsh
+++ zsh/lazy-sources/abbreviations.zsh
@@ -6,50 +6,50 @@ if ! (( $+commands[abbr] )) && ! (( $+functions[abbr] )); then
   return
 fi
 
-abbr -S less="less -giMRSW -z-4 -x4"
-abbr -S where="command -v"
-abbr -S df="df -h"
-abbr -S du="du -h"
-abbr -S tailf="tail -f"
-abbr -S pk='pkill -9 -f '
+abbr -f -S less="less -giMRSW -z-4 -x4"
+abbr -f -S where="command -v"
+abbr -f -S df="df -h"
+abbr -f -S du="du -h"
+abbr -f -S tailf="tail -f"
+abbr -f -S pk='pkill -9 -f '
 
-abbr -S cat='bat'
-abbr -S top='btop'
-abbr -S l='eza -la'
-abbr -S ll='eza -la'
-abbr -S ls='eza'
-abbr -S la='eza -la'
+abbr -f -S cat='bat'
+abbr -f -S top='btop'
+abbr -f -S l='eza -la'
+abbr -f -S ll='eza -la'
+abbr -f -S ls='eza'
+abbr -f -S la='eza -la'
 
 # tricks
-abbr -S path='echo -e ${PATH//:/\\n}'
-abbr -S myip="ip -br -c a"
-abbr -S ports="lsof -i -P -n | grep LISTEN"
-abbr -S wifi="networksetup -setairportpower en0"  # wifi on/off
+abbr -f -S path='echo -e ${PATH//:/\\n}'
+abbr -f -S myip="ip -br -c a"
+abbr -f -S ports="lsof -i -P -n | grep LISTEN"
+abbr -f -S wifi="networksetup -setairportpower en0"  # wifi on/off
 
 # git
-abbr -S ga="git add"
-abbr -S gap="git add -p"
-abbr -S gst="git status -sb ."
-abbr -S gc="git commit -m \""
-abbr -S gd="git diff"
-abbr -S gdc="git diff --cached"
-abbr -S gd1="git diff HEAD~1"
-abbr -S gd2="git diff HEAD~2"
-abbr -S gd3="git diff HEAD~3"
+abbr -f -S ga="git add"
+abbr -f -S gap="git add -p"
+abbr -f -S gst="git status -sb ."
+abbr -f -S gc="git commit -m \""
+abbr -f -S gd="git diff"
+abbr -f -S gdc="git diff --cached"
+abbr -f -S gd1="git diff HEAD~1"
+abbr -f -S gd2="git diff HEAD~2"
+abbr -f -S gd3="git diff HEAD~3"
 
 # dev
-abbr -S vim="nvim"
-abbr -S vi="nvim"
-abbr -S ag="rg --hidden"
-abbr -S rg="rg --hidden"
-abbr -S d='docker'
-abbr -S dc='docker compose'
-abbr -S nx="nlx"
-abbr -S hg="hg --encoding=utf-8"
-abbr -S typos="typos --config ~/.config/typos.toml ."
+abbr -f -S vim="nvim"
+abbr -f -S vi="nvim"
+abbr -f -S ag="rg --hidden"
+abbr -f -S rg="rg --hidden"
+abbr -f -S d='docker'
+abbr -f -S dc='docker compose'
+abbr -f -S nx="nlx"
+abbr -f -S hg="hg --encoding=utf-8"
+abbr -f -S typos="typos --config ~/.config/typos.toml ."
 
 # Mac
-abbr -S chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-abbr -S flusdns="dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
+abbr -f -S chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+abbr -f -S flusdns="dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
 
 # vim: set syntax=zsh:
