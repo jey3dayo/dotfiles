5c03bc40a4c1df805ed897bb24642484e0cb03dd
jey3dayo <nakazato_junya@ca-adv.co.jp>
2025-07-12 03:10:09 +0900

    feat(vim-fugitive): add hunk operations with GitSigns

--- .vim/lua/config/vim-fugitive.lua
+++ .vim/lua/config/vim-fugitive.lua
@@ -1,32 +1,72 @@
 local utils = require "core.utils"
-local gitlinker = utils.safe_require "gitlinker"
-if not gitlinker then return end
 
 -- Direct <C-g> keymaps without [git] prefix
 vim.keymap.set("n", "<C-g><C-g>", "<cmd>DiffviewOpen<CR>", { desc = "DiffviewOpen" })
 
--- Git status (moved from ,G)  
+-- Git status (moved from ,G)
 vim.keymap.set("n", "<C-g>s", function()
   local ok, mini_extra = pcall(require, "mini.extra")
   if ok then
     mini_extra.pickers.git_files()
   else
-    vim.cmd("Git")
+    vim.cmd "Git"
   end
 end, { desc = "Git status" })
 
-vim.keymap.set("n", "<C-g>a", "<cmd>Git add %<CR>", { desc = "Git add current file" })
+vim.keymap.set("n", "<C-g>a", "<cmd>Gwrite<CR>", { desc = "Git add current file" })
 vim.keymap.set("n", "<C-g>b", "<cmd>Git blame<CR>", { desc = "Git blame" })
-vim.keymap.set("n", "<C-g>d", "<cmd>Gdiffsplit<CR>", { desc = "Git diff split" })
-vim.keymap.set("n", "<C-g>h", "<cmd>DiffviewFileHistory<CR>", { desc = "File history" })
-vim.keymap.set("n", "<C-g>H", "<cmd>DiffviewFileHistory %<CR>", { desc = "Current file history" })
-vim.keymap.set("n", "<C-g>e", "<cmd>DiffviewFocusFiles<CR>", { desc = "Focus files" })
-vim.keymap.set("n", "<C-g>r", "<cmd>DiffviewOpen<CR>", { desc = "Reopen diff" })
-vim.keymap.set("n", "<C-g>g", "<cmd>Ggrep<Space>", { desc = "Git grep" })
-vim.keymap.set("n", "<C-g>w", "<cmd>Gwrite<CR>", { desc = "Git write" })
-vim.keymap.set("n", "<C-g>x", "<cmd>DiffviewClose<CR>", { desc = "Close diff" })
-vim.keymap.set("n", "<C-g>y", '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>', { desc = "Copy git link" })
-vim.keymap.set("n", "<C-g>Y", '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', { desc = "Open git link" })
+vim.keymap.set("n", "<C-g>d", "<cmd>Gdiffsplit<CR>", { desc = "Git diff split (current file)" })
+vim.keymap.set("n", "<C-g>D", "<cmd>DiffviewOpen<CR>", { desc = "Diffview open (全体)" })
+-- Hunk operations - <C-g>h as main entry point
+vim.keymap.set("n", "<C-g>h", function()
+  local gitsigns_ok, gitsigns = pcall(require, "gitsigns")
+  if not gitsigns_ok then
+    vim.notify("GitSigns not loaded", vim.log.levels.WARN)
+    return
+  end
 
-vim.keymap.set("v", "<C-g>y", '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>', { desc = "Copy git link" })
-vim.keymap.set("v", "<C-g>Y", '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', { desc = "Open git link" })
+  -- Check if GitSigns is attached to current buffer
+  if not gitsigns.get_hunks() or #gitsigns.get_hunks() == 0 then
+    vim.notify("No hunks found in current buffer", vim.log.levels.INFO)
+    return
+  end
+
+  gitsigns.stage_hunk()
+end, { desc = "Stage current hunk" })
+
+vim.keymap.set("n", "<C-g>r", function()
+  local gitsigns_ok, gitsigns = pcall(require, "gitsigns")
+  if not gitsigns_ok then
+    vim.notify("GitSigns not loaded", vim.log.levels.WARN)
+    return
+  end
+  
+  -- Check if GitSigns is attached to current buffer
+  if not gitsigns.get_hunks() or #gitsigns.get_hunks() == 0 then
+    vim.notify("No hunks found in current buffer", vim.log.levels.INFO)
+    return
+  end
+  
+  gitsigns.reset_hunk()
+end, { desc = "Reset current hunk" })
+vim.keymap.set("n", "<C-g>x", "<cmd>DiffviewClose<CR>", { desc = "Close diff" })
+
+-- Hunk navigation (n/p pattern like vim search)
+vim.keymap.set("n", "<C-g>n", function()
+  local gitsigns_ok, gitsigns = pcall(require, "gitsigns")
+  if not gitsigns_ok then
+    vim.notify("GitSigns not loaded", vim.log.levels.WARN)
+    return
+  end
+  gitsigns.next_hunk()
+end, { desc = "Next hunk" })
+
+vim.keymap.set("n", "<C-g>p", function()
+  local gitsigns_ok, gitsigns = pcall(require, "gitsigns")
+  if not gitsigns_ok then
+    vim.notify("GitSigns not loaded", vim.log.levels.WARN)
+    return
+  end
+  gitsigns.prev_hunk()
+end, { desc = "Previous hunk" })
+
