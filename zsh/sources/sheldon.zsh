09a59247ee6b7a91305bf0de5b2e55d72fd8c8ab
jey3dayo <j138cm@gmail.com>
2025-07-21 14:26:50 +0900

    fix(zsh): check sheldon command before caching

--- zsh/sources/sheldon.zsh
+++ zsh/sources/sheldon.zsh
@@ -6,10 +6,12 @@ sheldon_cache="$SHELDON_CONFIG_DIR/sheldon.zsh"
 sheldon_toml="$SHELDON_CONFIG_DIR/plugins.toml"
 
 # キャッシュがない、またはキャッシュが古い場合にキャッシュを作成
-if [[ ! -r "$sheldon_cache" || "$sheldon_toml" -nt "$sheldon_cache" ]]; then
-  mkdir -p $cache_dir
-  sheldon source >$sheldon_cache
+if command -v sheldon >/dev/null 2>&1; then
+  if [[ ! -r "$sheldon_cache" || "$sheldon_toml" -nt "$sheldon_cache" ]]; then
+    mkdir -p $cache_dir
+    sheldon source >$sheldon_cache
+  fi
+  source "$sheldon_cache"
 fi
-source "$sheldon_cache"
 
 unset cache_dir sheldon_cache sheldon_toml
