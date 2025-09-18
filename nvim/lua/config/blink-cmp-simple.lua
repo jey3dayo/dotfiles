dc6e80ff9b06e2e78d7ddb8d7713cdf733f20088
jey3dayo <nakazato_junya@ca-adv.co.jp>
2025-09-18 19:17:02 +0900

    refactor(config): update frecency setup in blink-cmp

--- nvim/lua/config/blink-cmp-simple.lua
+++ nvim/lua/config/blink-cmp-simple.lua
@@ -33,7 +33,7 @@ require("blink.cmp").setup {
 
   fuzzy = {
     implementation = "prefer_rust_with_warning",
-    use_frecency = true,
+    frecency = { enabled = true },
     use_proximity = true,
   },
 }
